a8a671ff0d5d7aa7acc0d262b1a78289ca921732
R-FUETA <raul.a.fuenzalida@gmail.com>
2025-05-15 23:09:29 -0400

    Primer commit: estructura inicial del proyecto

---
+++ src/main/java/com/edutech_innovators_spa/cl/edutech_innovators_spa/EdutechInnovatorsSpaApplication.java
@@ -0,0 +1,13 @@
+package com.edutech_innovators_spa.cl.edutech_innovators_spa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutechInnovatorsSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdutechInnovatorsSpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edutech_innovators_spa/cl/edutech_innovators_spa/EdutechInnovatorsSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutech_innovators_spa.cl.edutech_innovators_spa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdutechInnovatorsSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
